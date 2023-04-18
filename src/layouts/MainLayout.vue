3dbad89e26a79f9be4a032ea17fc78800249557b
marcos <marcos@slice.global>
2023-04-18 16:54:14 -0300

    posicionamento header

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,22 +1,5 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
-
-        <q-toolbar-title> Quasar App </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
-      </q-toolbar>
-    </q-header>
-
     <q-page-container>
       <router-view />
     </q-page-container>
@@ -25,7 +8,6 @@
 
 <script>
 import { defineComponent, ref } from "vue";
-import EssentialLink from "components/EssentialLink.vue";
 
 const linksList = [
   {
@@ -62,15 +44,11 @@ export default defineComponent({
   name: "MainLayout",
 
   components: {
-    // EssentialLink,
   },
 
   setup() {
-    // const leftDrawerOpen = ref(false);
 
     return {
-      // essentialLinks: linksList,
-      // leftDrawerOpen,
 
     };
   },
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,10 @@
 <template>
   <q-page style="background-color: #fafafc" class="flex flex-center">
-    <q-header style="background-color: #fafafc; border: none" elevated>
-      <q-toolbar>
-        <q-space></q-space>
-        <q-btn
-          flat
-          round
-          size="17px"
-          class="text-bold"
-          style="background-color: #d9d9d9; margin: 30px"
-          label="J"
-        />
-      </q-toolbar>
+    <div
+      class="justify-between row"
+      style="width: 100%; max-width: 1700px position: fixed;"
+      elevated
+    >
       <q-toolbar-title>
         <img
           alt="Quasar logo"
@@ -19,26 +12,51 @@
           style="width: 200px; margin: 30px"
         />
       </q-toolbar-title>
-    </q-header>
+      <div>
+        <q-btn
+          flat
+          round
+          size="17px"
+          class="text-bold"
+          style="background-color: #d9d9d9; margin-top: 10px; margin-right: 40px;"
+          label="J"
+        />
+      </div>
+    </div>
 
     <div>
-      <h4 style="margin: auto; margin-bottom: 8px;" class="text-left text-bold">Olá João,</h4>
-      <h6 style="margin: auto; margin-bottom: 25px; font-size: 1.1rem;" class="text-left text-grey-8">Seja bem vindo a sua conta de hospedagem</h6>
-      <iframe
-        width="1158"
-        height="660"
-        src="https://www.youtube.com/embed/Ywh8NVAHswU"
-        title="Como criar um servidor no Discord? | Locaweb"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        allowfullscreen
-      ></iframe>
+      <h4 style="margin: auto; margin-bottom: 8px" class="text-left text-bold">
+        Olá João,
+      </h4>
+      <h6
+        style="margin: auto; margin-bottom: 25px; font-size: 1.1rem"
+        class="text-left text-grey-8"
+      >
+        Seja bem vindo a sua conta de hospedagem
+      </h6>
+      <div class="flex flex-center" style="widows: 900px">
+        <iframe
+          width="900"
+          height="500"
+          src="https://www.youtube.com/embed/Ywh8NVAHswU"
+          title="Como criar um servidor no Discord? | Locaweb"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen
+        ></iframe>
+      </div>
       <p
-              style="margin: auto; margin-bottom: 20px; font-size: 1rem; margin-top: 20px;"
-              class="text-left "
-            >
-              Caso tenha alguma dúvida, <a style="color:black" href=""> visite nossa central de ajuda.</a>
-    </p>
+        style="
+          margin: auto;
+          margin-bottom: 20px;
+          font-size: 1rem;
+          margin-top: 20px;
+        "
+        class="text-left"
+      >
+        Caso tenha alguma dúvida,
+        <a style="color: black" href=""> visite nossa central de ajuda.</a>
+      </p>
     </div>
   </q-page>
 </template>
