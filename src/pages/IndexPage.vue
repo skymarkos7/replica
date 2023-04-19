2c46c48c9b5852271acda16833f0e8d97354bad2
marcos <marcos@slice.global>
2023-04-19 07:45:11 -0300

    btn sair

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -2,30 +2,43 @@
   <q-page style="background-color: #fafafc" class="flex flex-center">
     <div
       class="justify-between row"
-      style="width: 100%; max-width: 1200px"
-      elevated
+      style="width: 100%; max-width: 1200px; margin: 0 auto"
     >
       <q-toolbar-title>
         <img
           alt="Quasar logo"
           src="~assets/img/Logo.png"
-          style="width: 200px; margin: 30px"
+          style="width: 136px; margin: 30px"
         />
       </q-toolbar-title>
-      <div>
+      <div class="column">
         <q-btn
           flat
           round
           size="17px"
           class="text-bold"
-          style="background-color: #d9d9d9; margin-top: 10px; margin-right: 40px;"
+          style="
+            background-color: #d9d9d9;
+            margin-top: 10px;
+            margin-right: 40px;
+          "
           label="J"
+          @click="logout()"
         />
+
+        <a
+          style="text-decoration: none; margin-top: 20px; margin-left: 10px"
+          href="#/login"
+          ><p v-if="sair">Sair</p></a
+        >
       </div>
     </div>
 
     <div>
-      <h4 style="margin: auto; margin-bottom: 8px" class="text-left text-bold">
+      <h4
+        style="margin: 0 auto; margin-bottom: 8px"
+        class="text-left text-bold"
+      >
         Olá João,
       </h4>
       <h6
@@ -34,10 +47,10 @@
       >
         Seja bem vindo a sua conta de hospedagem
       </h6>
-      <div class="flex flex-center" style="widows: 900px">
+      <div class="flex flex-center">
         <iframe
-          width="900"
-          height="500"
+          width="976"
+          height="654"
           src="https://www.youtube.com/embed/Ywh8NVAHswU"
           title="Como criar um servidor no Discord? | Locaweb"
           frameborder="0"
@@ -66,5 +79,20 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "IndexPage",
+  data() {
+    return {
+      sair: false,
+    };
+  },
+  methods: {
+    logout() {
+      if (this.sair==true) {
+        this.sair = false;
+      }else{
+        this.sair = true;
+      }
+
+    },
+  },
 });
 </script>
